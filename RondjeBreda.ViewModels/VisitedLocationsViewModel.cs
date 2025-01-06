f7cbe4128ca716ec20c87a1c52904ca15cc08926
LeonBuijs <77409149+LeonBuijs@users.noreply.github.com>
2025-01-06 09:16:12 +0100

    Begin gemaakt aan de visited locations page

--- RondjeBreda.ViewModels/VisitedLocationsViewModel.cs
+++ RondjeBreda.ViewModels/VisitedLocationsViewModel.cs
@@ -1,14 +1,24 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Android.OS;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using RondjeBreda.Domain.Interfaces;
+using RondjeBreda.Domain.Models.DatabaseModels;
 
 namespace RondjeBreda.ViewModels;
 
 /// <summary>
 /// The Viewmodel for the visitedlocationpage
 /// </summary>
-public class VisitedLocationsViewModel : ObservableObject
+public partial class VisitedLocationsViewModel : ObservableObject
 {
     private IDatabase database;
     private IPreferences preferences;
     // private ObservableList<Location> visitedLocations;
+
+    [RelayCommand]
+    private void RouteTapped(Route route)
+    {
+        // TODO
+        Console.WriteLine($"Route tapped: {route}");
+    }
 }
