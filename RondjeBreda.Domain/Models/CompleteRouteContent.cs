576f0f41d5596844dfca83b72b113cd4820d0816
davidVeer <davidvdveer05@gmail.com>
2025-01-06 12:35:22 +0100

    CompleteRouteConntent object added correctly

--- RondjeBreda.Domain/Models/CompleteRouteContent.cs
+++ RondjeBreda.Domain/Models/CompleteRouteContent.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 
 namespace RondjeBreda.Domain.Models
 {
-    internal class MaaktVoorNuNietUit
+
+    public class CompleteRouteContent
     {
+        public RouteLocation[] HistorischeKilometerRoute { get; set; }
     }
+
+    public class RouteLocation
+    {
+        [JsonProperty("fotoNr")]
+        public string PhotoNr { get; set; }
+
+        [JsonProperty("noorderBreedte")]
+        public string Latitude { get; set; }
+
+        [JsonProperty("oosterLengte")]
+        public string Longitude { get; set; }
+
+        [JsonProperty("naam")]
+        public string LocationName { get; set; }
+
+        [JsonProperty("opmerking")]
+        public string Comment { get; set; }
+
+        public int routeNr { get; set; }
+    }
+
 }
