d1bb53504a24854d845a75d33f835d04114aa90b
tiemenbr <tiemenbr@hotmail.nl>
2025-01-13 12:40:33 +0100

    dependencies edited for RondjeBredaTestProject

---
+++ RondjeBredaTestProject/RondjeBredaTests.cs
@@ -0,0 +1,12 @@
+﻿using RondjeBreda.Domain.Interfaces;
+using RondjeBreda.Infrastructure.DatabaseImplementation;
+
+namespace RondjeBredaTestProject {
+    [TestClass]
+    public sealed class RondjeBredaTests {
+        [TestMethod]
+        public void TestMethod1() {
+            
+        }
+    }
+}
